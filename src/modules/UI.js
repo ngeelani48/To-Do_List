b912325e93e7b7347c800f729ee532179850ea81
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-02-23 21:55:58 +0300

    update UI.js

--- src/modules/UI.js
+++ src/modules/UI.js
@@ -61,12 +61,3 @@ export const deleteTask = (e, ListArray) => {
   updateLocalStorage(ListArray);
   FnToDoList(ListArray);
 };
-
-export const markTask = (e, ListArray) => {
-  const clickedCheckbox = e.target.closest('.list-li-checkbox');
-  const clickedTask = clickedCheckbox.nextElementSibling;
-  const taskIndex = ListArray.findIndex((task) => task.task === clickedTask.value);
-  ListArray[taskIndex].completed = !ListArray[taskIndex].completed;
-  updateLocalStorage(ListArray);
-  FnToDoList(ListArray);
-};
